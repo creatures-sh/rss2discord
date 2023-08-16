0fc0a7b3be83f26f9e4749d2ab72a6cc943bfabc
Lazar Nikolov <hello@nikolovlazar.com>
2023-08-16 09:50:39 -0400

    documenting things

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -11,12 +11,18 @@ export const globalMetadata = sqliteTable('global_metadata', {
 
 export const feeds = sqliteTable('feeds', {
   id: integer('id').primaryKey({ autoIncrement: true }).notNull(),
+  // [FEED] the actual XML/Atom full URL
   feed_url: text('feed_url').notNull(),
+  // [USER] the URL that we got from the client
   page_url: text('page_url').notNull(),
+  // [FEED] the title of the publication
   title: text('title'),
+  // [FEED] the description of the publication
   description: text('description'),
+  // [FEED] the <link> property from the feed
   link: text('link'),
   last_item_guid: text('last_item_guid'),
   last_item_pub_date: text('last_item_pub_date'),
+  // [USER] user-submitted channel for this feed
   channel: text('channel'),
 });
